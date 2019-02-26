d5762ea1343f5ef42cceeaccce03fb535939393a
Phan Thanh Tu <phanthanhtu@luvina.net>
2019-02-26 16:05:15 +0700

    fragment home xml

--- app/src/main/java/com/example/mrtus/demoapp/feature/home/MainActivity.java
+++ app/src/main/java/com/example/mrtus/demoapp/feature/home/MainActivity.java
@@ -3,14 +3,12 @@ package com.example.mrtus.demoapp.feature.home;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-import com.example.mrtus.demoapp.BlankFragment;
+import com.example.mrtus.demoapp.HomeFragment;
 import com.example.mrtus.demoapp.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadFragment(new BlankFragment());
+        loadFragment(new HomeFragment());
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -29,11 +27,11 @@ public class MainActivity extends AppCompatActivity {
             Fragment fragment;
             switch (item.getItemId()) {
                 case R.id.navigation_new:
-                    fragment = new BlankFragment();
+                    fragment = new HomeFragment();
                     loadFragment(fragment);
                     return true;
                 case R.id.navigation_account:
-                    fragment = new BlankFragment();
+                    fragment = new HomeFragment();
                     loadFragment(fragment);
                     return true;
             }
